973bdffd9a6aa61a2523d9ca738fc6d7ed8bc3b5
Shrihari Prakash <shrihariprakash@outlook.com>
2020-07-22 21:55:29 +0530

    - Added a simple message exchange demo in backend/node/api/socket/v1/demo.html

--- backend/node/api/socket/v1/middleware.js
+++ backend/node/api/socket/v1/middleware.js
@@ -1,9 +1,21 @@
+var connectionMap = {};
 const socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log("a user connected");
-    //Sending an object when emmiting an event
-    socket.emit("testEvent", {
-      description: "Test event recieved!",
+    socket.on("_connect", (id) => {
+      console.log("connected: " + id.id);
+      socket.id = id.id;
+      connectionMap[socket.id] = socket;
+    });
+
+    socket.on("_messageOut", (message) => {
+      console.log(message)
+      connectionMap[message.receiver_id].emit("_messageIn", {
+        message: message.content,
+      });
+    });
+
+    socket.on("disconnect", (message) => {
+      delete connectionMap[socket.id];
     });
   });
 };
