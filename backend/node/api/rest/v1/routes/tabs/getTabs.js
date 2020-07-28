ef7d763d7e4bcbc55dd2396615deef68e3bc3c51
Shrihari Prakash <shrihariprakash@outlook.com>
2020-07-28 22:13:58 +0530

    - Implemented getTabs api to get a list of all tabs for the given thread.

---
+++ backend/node/api/rest/v1/routes/tabs/getTabs.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+var ObjectId = require("mongodb").ObjectID;
+
+const tokenMgr = require("../../../../utils/tokenManager");
+const tokenManager = new tokenMgr.tokenManager();
+
+const errorBuilder = require("../../../../utils/responseErrorBuilder");
+
+router.get("/", async function (req, res) {
+  getThreads(req, res);
+});
+
+async function getThreads(req, res) {
+  let authToken = req
+    .header("authorization")
+    .slice(7, req.header("authorization").length)
+    .trimLeft();
+
+  if (!authToken)
+    return res.status(403).json({
+      status: "ERR",
+      reason: errorBuilder.buildReason("unauthorized"),
+      insight: errorBuilder.buildInsight("unauthorized"),
+    });
+
+  let cacheManager = req.app.get("cacheManager");
+
+  let db = req.app.get("mongoInstance");
+
+  let loggerInUser = await tokenManager.verify(db, authToken, cacheManager);
+
+  if (!loggerInUser)
+    return res.status(404).json({
+      status: "ERR",
+      reason: errorBuilder.buildReason("unauthorized"),
+      insight: errorBuilder.buildInsight("unauthorized"),
+    });
+
+  if (!req.query.thread_id)
+    return res.status(403).json({
+      status: "ERR",
+      reason: errorBuilder.buildReason("empty", "THREAD_ID"),
+      insight: errorBuilder.buildInsight("empty", "thread id"),
+    });
+
+  db.collection("threads").findOne(
+    { _id: ObjectId(req.query.thread_id) },
+    function (err, threadObject) {
+      if (err)
+        return res.status(400).json({
+          status: "ERR",
+          reason: errorBuilder.buildReason("invalid", "THREAD_ID"),
+          insight: errorBuilder.buildInsight("invalid", "thread id"),
+        });
+      console.log(threadObject.thread_participants, loggerInUser.user_id);
+      var hasAccess = threadObject.thread_participants.some(function (
+        participantId
+      ) {
+        return participantId.equals(loggerInUser.user_id);
+      });
+      if (!hasAccess)
+        return res.status(404).json({
+          status: "ERR",
+          reason: errorBuilder.buildReason("unauthorized"),
+          insight: errorBuilder.buildInsight("unauthorized"),
+        });
+
+      db.collection("tabs")
+        .find({
+          _id: { $in: threadObject.tabs },
+        })
+        .toArray(function (err, tabObject) {
+          console.log(tabObject);
+          return res.status(200).json({
+            status: "SUCCESS",
+            message: "Tabs Retrieved.",
+            tabs: tabObject,
+          });
+        });
+    }
+  );
+}
+
+module.exports = router;
--- backend/node/api/rest/v1/routes/tabs/tabsMiddleware.js
+++ backend/node/api/rest/v1/routes/tabs/tabsMiddleware.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const newTab = require("./newTab");
-/* const getTabs = require("./getTabs") */
+const getTabs = require("./getTabs")
 
 
 router.use("/newTab", newTab);
 
-/* router.use("/getThreads", getThreads); */
+router.use("/getTabs", getTabs);
 
 module.exports = router;
