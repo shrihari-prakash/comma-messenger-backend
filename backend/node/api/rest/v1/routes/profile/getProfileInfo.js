860d34e70250c45d563c8ec3e153a9c999be67ea
Shrihari Prakash <shrihariprakash@outlook.com>
2020-08-04 22:27:52 +0530

    - Added getProfileInfo route.

---
+++ backend/node/api/rest/v1/routes/profile/getProfileInfo.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+var ObjectId = require("mongodb").ObjectID;
+
+const tokenMgr = require("../../../../utils/tokenManager");
+const tokenManager = new tokenMgr.tokenManager();
+
+const errors = require("../../../../utils/errors");
+const errorModel = require("../../../../utils/errorResponse");
+
+router.get("/", async function (req, res) {
+  getThreads(req, res);
+});
+
+async function getThreads(req, res) {
+  //Start of input validation.
+  if (!req.header("authorization")) {
+    let error = new errorModel.errorResponse(errors.invalid_key);
+    return res.status(403).json(error);
+  }
+
+  let authToken = req
+    .header("authorization")
+    .slice(7, req.header("authorization").length)
+    .trimLeft();
+
+  if (!authToken) {
+    let error = new errorModel.errorResponse(errors.invalid_key);
+    return res.status(403).json(error);
+  }
+
+  let cacheManager = req.app.get("cacheManager");
+
+  let db = req.app.get("mongoInstance");
+
+  let loggedInUserId = await tokenManager.verify(db, authToken, cacheManager);
+
+  if (!loggedInUserId) {
+    let error = new errorModel.errorResponse(errors.invalid_key);
+    return res.status(403).json(error);
+  }
+  //End of input validation.
+
+  try {
+    var userObject = await db.collection("users").findOne({
+      _id: ObjectId(loggedInUserId),
+    }, { projection: { _id: 0, threads: 0 } });
+    return res.status(200).json({
+      status: 200,
+      message: "User Profile Retrieved.",
+      result: userObject,
+    });
+  } catch (e) {
+    let error = new errorModel.errorResponse(errors.internal_error);
+    return res.json(error);
+  }
+}
+
+module.exports = router;
--- backend/node/api/rest/v1/routes/profile/profileMiddleware.js
+++ backend/node/api/rest/v1/routes/profile/profileMiddleware.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const editProfileInfo = require("./editProfileInfo");
+const getProfileInfo = require("./getProfileInfo");
 
 router.use("/editProfileInfo", editProfileInfo);
+
+router.use("/getProfileInfo", getProfileInfo);
 
 module.exports = router;
