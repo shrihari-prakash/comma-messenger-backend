fb8d2821a98734c9cc8936dd777fbbbc7f423172
Shrihari Prakash <shrihariprakash@outlook.com>
2020-10-04 22:06:33 +0530

    - Added profile picture in getThreads API result.

--- rest/v1/routes/threads/getThreads.js
+++ rest/v1/routes/threads/getThreads.js
@@ -85,7 +85,7 @@ async function getThreads(req, res) {
                       _id: 1,
                       email: 1,
                       name: 1,
-                      profile_picture: 1,
+                      display_picture: 1,
                     },
                   },
                 ],
