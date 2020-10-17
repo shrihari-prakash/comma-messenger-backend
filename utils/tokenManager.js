ca5b22c4c0bb985fd240795e1b43e77600c6239f
Shrihari Prakash <shrihariprakash@outlook.com>
2020-10-17 21:16:17 +0530

    - Better token generation.

--- utils/tokenManager.js
+++ utils/tokenManager.js
@@ -1,4 +1,5 @@
 const { ObjectId } = require("mongodb");
+const crypto = require("crypto");
 
 function tokenManager() {
   this.generate = (db, userId, cacheManager) => {
@@ -9,7 +10,7 @@ function tokenManager() {
     return new Promise((resolve, reject) => {
       let now = new Date();
       let tomorrow = new Date(new Date().setDate(now.getDate() + 1));
-      let insertToken = token();
+      let insertToken = "CM_" + token(125);
 
       let tokenObject = {
         user_id: ObjectId(userId),
@@ -38,7 +39,9 @@ function tokenManager() {
       let userId = cacheManager.getUserIdFromToken(token);
 
       if (userId) {
-        console.log("User " + userId + "'s token has been verified from cache.");
+        console.log(
+          "User " + userId + "'s token has been verified from cache."
+        );
         //If the token validation is success, we bump up the token expiry time to one more day so that the user stays logged in.
         db.collection("tokens").updateOne(
           { token: token },
@@ -52,7 +55,7 @@ function tokenManager() {
           .findOne({ token: token });
 
         if (!tokenObject) {
-          console.log("No valid token was matched for the given request.")
+          console.log("No valid token was matched for the given request.");
           return resolve(false);
         }
 
@@ -65,7 +68,11 @@ function tokenManager() {
             { $set: { date_expiry: tomorrow } }
           );
 
-        console.log("User " + tokenObject.user_id + "'s token has been verified from database.");
+        console.log(
+          "User " +
+            tokenObject.user_id +
+            "'s token has been verified from database."
+        );
         cacheManager.putUserToken(token, tokenObject.user_id.toString());
         resolve(tokenObject.user_id.toString());
       }
@@ -73,16 +80,13 @@ function tokenManager() {
   };
 }
 
-function randomizer() {
-  return Math.random().toString(36).substr(2); // remove `0.`
-}
-
-function dateHash() {
-  return (+new Date()).toString(36);
-}
-
-function token() {
-  return randomizer() + randomizer() + randomizer() + "_" + dateHash();
+//function code taken from http://blog.tompawlak.org/how-to-generate-random-values-nodejs-javascript
+function token(len) {
+  return crypto
+    .randomBytes(Math.ceil(len / 2))
+    .toString("hex") // convert to hexadecimal format
+    .slice(0, len)
+    .toUpperCase(); // return required number of characters
 }
 
 module.exports = new tokenManager();
