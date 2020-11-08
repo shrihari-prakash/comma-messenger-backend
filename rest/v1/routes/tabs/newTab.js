cd016d2b2838a5a782e7cc7cd0a97f518e66b443
Shrihari Prakash <shrihariprakash@outlook.com>
2020-11-08 11:58:12 +0530

    - Fixed checking wrong collection while capping tab creation.

--- rest/v1/routes/tabs/newTab.js
+++ rest/v1/routes/tabs/newTab.js
@@ -84,7 +84,7 @@ async function createThread(req, res) {
 
   try {
     var tabCount = await db
-      .collection("tab")
+      .collection("tabs")
       .find({ thread_id: ObjectId(tabDetails.thread_id) })
       .count();
 
